cda251bdca1b47ccdc15ac943c9669ecd3215bb6
Guilherme dos anjos silva <165790861+guilhermedosanjosilva77@users.noreply.github.com>
2024-09-13 09:50:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const vermelho = document.getElementById('vermelho')
+const amarelo = document.getElementById('amarelo')
+const verde = document.getElementById('verde')
+
+let estadoAtual = 'amarelo'
+
+function alternarLuzes(){
+    if(estadoAtual === 'amarelo'){
+        vermelho.style.backgroundColor ='#55555';
+        amarelo.style.backgroundColor = '#d6d632';
+        verde.style.backgroundColor = '#555555';
+        estadoAtual = 'vermelho';
+
+    } else if (estadoAtual === 'vermelho'){
+        vermelho.style.backgroundColor = '#ff0000';
+        amarelo.style.backgroundColor = '#555555';
+        verde.style.backgroundColor = '#555555';
+        estadoAtual = 'verde';
+    }else if(estadoAtual === 'verde'){
+        vermelho.style.backgroundColor = '#555555';
+        amarelo.style.backgroundColor = '#555555';
+        verde.style.backgroundColor = '#00ff00';
+        estadoAtual = 'amarelo';
+
+    }
+}
+setInterval(alternarLuzes,2000);
+});
